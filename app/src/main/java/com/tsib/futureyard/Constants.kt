88d6f4b788a934de69e3aa37cf595fa172bc65fb
Tatyana Shkolnik <shkolniktatyana21@gmail.com>
2020-12-10 23:47:19 +0300

    New content.

--- app/src/main/java/com/tsib/futureyard/Constants.kt
+++ app/src/main/java/com/tsib/futureyard/Constants.kt
@@ -5,14 +5,15 @@ import android.graphics.drawable.Drawable
 object Constants {
     const val TAG = "CHECKER"
     const val CAMERA = "CameraFragment:"
-    const val AR_RECYCLE_SIZE = 5
+    const val AR_RECYCLE_SIZE = 6
 
     val photos: Array<Int> = arrayOf(
         R.drawable.ic_trash,
         R.drawable.ic_light,
         R.drawable.ic_bench,
         R.drawable.ic_flowerbed,
-        R.drawable.ic_bush
+        R.drawable.ic_bush,
+        R.drawable.ic_kianu
     )
 
     val titles = arrayOf(
@@ -20,20 +21,23 @@ object Constants {
         "Фонарь",
         "Скамейка",
         "Клумба",
-        "Куст"
+        "Куст",
+        "Киану Ривз"
     )
     val subtitles = arrayOf(
         "пластик",
         "железо",
         "дерево",
         "камни",
-        "дерево"
+        "дерево",
+        "брутальность"
     )
     val descriptions = arrayOf(
         "Cпециализированная ёмкость, служащая для сбора бытовых или строительных отходов. ",
         "Cтационарный источник света. Прибор для освещения отдельных участков пространства в темное время суток.",
         "Деревянное приспособление для сидения, размещаемое на открытом воздухе и достпуное каждому.",
         "Часть цветника, обычно в виде какой - либо геометрической. фигуры, имеющая замкнутую форму.",
-        "Растение с древовидными ветвями, начинающимися почти от самой поверхности земли."
+        "Растение с древовидными ветвями, начинающимися почти от самой поверхности земли.",
+        "Неподражаемый Киану Ривз ждёт вас в удивительном мире киберпанка."
     )
 }
